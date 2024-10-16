3ceb0a986da72343265bbdba61ce6cd7a26cb115
EemeliJ <Eemeli.JJ@outlook.com>
2024-10-16 16:48:47 +0300

    Add files via upload

---
+++ Fysproj1.py
@@ -0,0 +1,144 @@
+# %%
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import streamlit as st
+import scipy.signal as signal
+import folium
+import streamlit.components.v1 as components
+
+st.markdown("<h1 style='text-align: center; color: black;'>Iltalenkki</h1>", unsafe_allow_html=True) 
+
+df = pd.read_csv('Accelerometer.csv')
+
+# Z KOMPONENTTI JA SUODATUS
+st.markdown("<h2 style='text-align: center; color: black;'>Suodatettu Z-komponentti</h2>", unsafe_allow_html=True)
+
+step_comp = df['Acceleration z (m/s^2)']
+
+def butter_lowpass_filter(data, cutoff, fs, order):
+    nyq = fs / 2.0
+    normal_cutoff = cutoff / nyq
+    b, a = signal.butter(order, normal_cutoff, btype='low', analog=False)
+    y = signal.filtfilt(b, a, data)
+    return y
+
+T = df['Time (s)'].iloc[-1] - df['Time (s)'].iloc[0]
+n = len(df['Time (s)'])
+fs = n / T  
+
+
+order = 3
+cutoff = 1 / (0.2)
+
+filtered_signal = butter_lowpass_filter(step_comp, cutoff, fs, order)
+
+start_time = 60
+end_time = 120
+mask = (df['Time (s)'] >= start_time) & (df['Time (s)'] <= end_time)
+
+filtered_signal_short = filtered_signal[mask]
+time_short = df['Time (s)'][mask]
+
+plt.figure(figsize=(20, 4))
+plt.plot(time_short, filtered_signal_short, color='purple')
+plt.grid()
+plt.axis([start_time, end_time, -2, 22])
+# plt.title('Suodatettu Kiihtyvyys Z (60-120 s)')
+plt.xlabel('Aika (s)')
+plt.ylabel('Kiihtyvyys (m/s²)')
+plt.legend()
+
+st.pyplot(plt)
+plt.clf()
+
+# SUODATETTU ASKELMÄÄRÄ
+filtered_signal_mean_removed = filtered_signal - np.mean(filtered_signal)
+
+jaksot = 0
+for i in range(1, len(filtered_signal_mean_removed)):
+    if filtered_signal_mean_removed[i] * filtered_signal_mean_removed[i - 1] < 0:
+        jaksot += 1
+
+st.write('Askelmäärä suodatuksen perusteella', np.floor(jaksot / 2))
+
+# TEHOSPEKTRI
+st.markdown("<h2 style='text-align: center; color: black;'>Tehospektri</h2>", unsafe_allow_html=True)
+
+filtered_signal_short -= np.mean(filtered_signal_short)
+n = len(filtered_signal_short)
+frequencies = np.fft.fftfreq(n, 1/fs)
+fft_values = np.fft.fft(filtered_signal_short)
+
+power = np.abs(fft_values)**2 / n 
+
+positive_frequencies = frequencies[:n//2]
+positive_power = power[:n//2]
+
+freq_mask = (positive_frequencies > 0) & (positive_frequencies <= 14)
+filtered_frequencies = positive_frequencies[freq_mask]
+filtered_power = positive_power[freq_mask]
+
+filtered_power[filtered_power > 450] = 450
+
+plt.figure(figsize=(10, 6))
+plt.plot(filtered_frequencies, filtered_power, color='purple')
+# plt.title(f'Tehospektri ({start_time}-{end_time} s)')
+plt.xlabel('Taajuus (Hz)')
+plt.ylabel('Teho')
+plt.grid()
+plt.axis([0, 14, 0, 450])
+
+st.pyplot(plt)
+plt.clf()
+
+# ASKELMÄÄRÄ FOURIER-ANALYYSI
+step_count_from_power = 0
+for f, p in zip(filtered_frequencies, filtered_power):
+    if 1 <= f <= 3:
+        step_count_from_power += p
+
+step_count_from_power = step_count_from_power / 7
+
+st.write('Askelmäärä Fourier-analyysin perusteella:', np.floor(step_count_from_power))
+
+# KARTTA
+st.markdown("<h2 style='text-align: center; color: black;'>Reitti kartalla</h2>", unsafe_allow_html=True)
+
+df = pd.read_csv('Location.csv')
+
+df.columns = df.columns.str.strip()
+
+lat_mean = df['Latitude (°)'].mean()
+long_mean = df['Longitude (°)'].mean()
+
+my_map = folium.Map(location=[lat_mean, long_mean], zoom_start=14)
+
+start_time = 10
+mask = df['Time (s)'] >= start_time
+
+folium.PolyLine(df.loc[mask, ['Latitude (°)', 'Longitude (°)']], color='purple', opacity=1).add_to(my_map)
+
+map_file = 'map.html'
+my_map.save(map_file)
+
+with open(map_file, 'r', encoding='utf-8') as f:
+    map_html = f.read()
+
+components.html(map_html, height=600)
+
+# KESKIMÄÄRÄINEN NOPEUS (m/s) ASKELPITUUS (cm) JA MATKA
+
+df['Distance (m)'] = np.sqrt((df['Latitude (°)'].diff() * 111320)**2 + (df['Longitude (°)'].diff() * 111320 * np.cos(np.radians(df['Latitude (°)']))**2)**2)
+
+total_distance = df['Distance (m)'].sum()
+total_time = df['Time (s)'].iloc[-1] - df['Time (s)'].iloc[0]
+
+average_speed = total_distance / total_time if total_time > 0 else 0
+
+step_length = total_distance / (np.floor(jaksot / 2)) * 100
+
+st.write(f'Keskimääräinen nopeus (m/s): {average_speed:.1f}')
+st.write(f'Askelpituus (cm): {step_length:.1f}')
+st.write(f'Kokonaismatka (km): {total_distance / 1000:.1f}')
+
